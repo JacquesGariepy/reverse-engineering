6275589648ef2b8fd155212f2dd2fc0712d09e25
Jacques Gariépy <jacques.gariepy@outlook.com>
2024-08-15 15:27:05 -0400

    Update __init__.py

--- reverseEngineer/__init__.py
+++ reverseEngineer/__init__.py
@@ -1,6 +1,30 @@
-from .config import Config, ModelConfig
-from .exceptions import ReverseEngineerError
-from .keys_manager import KeyManager
-from .llm_manager import LLMManager
+
+# Expose key classes and modules for easier access when importing the package
+
 from .reverse_engineer import ReverseEngineer
-from .utils import read_file, is_url, load_env_variables, find_test_files
+from .exceptions import ReverseEngineerError
+from .config import Config, ModelConfig
+from .utils import (
+    read_file,
+    _is_url,
+    _read_local_file,
+    _read_url
+)
+from .llm_manager import LLMManager
+from .keys_manager import KeyManager
+
+__all__ = [
+    "ReverseEngineer",
+    "ReverseEngineerError",
+    "Config",
+    "ModelConfig",
+    "read_file",
+    "_is_url",
+    "_read_local_file",
+    "_read_url",
+    "LLMManager",
+    "KeyManager"
+]
+
+# Version of the reverse_engineer package
+__version__ = "0.1.0"
